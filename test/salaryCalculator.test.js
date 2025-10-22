8c6b0651f289bcfd0bf92ec033b5ae1392c9ac3f
rysardP <rysard.poplavskij@ehu.lt>
2025-10-22 20:25:24 +0300

    add unit test

--- test/salaryCalculator.test.js
+++ test/salaryCalculator.test.js
@@ -7,7 +7,7 @@ import Fakerator from 'fakerator';
 const fakerator = Fakerator();
 
 describe("calculateProposedSalary", () => {
-  it("should calculate salary with valid inputs", () => {
+  it("should calculate salary correctly with valid inputs", () => {
     const value = fakerator.random.number(200, 500, 2);
     const result = calculateProposedSalary({
       value: value,
@@ -19,6 +19,42 @@ describe("calculateProposedSalary", () => {
       currencyId: "978",
       periodId: 4,
     });
+  });
+
+  it("should throw error for invalid salary value", () => {
+    expect(() => calculateProposedSalary({
+      value: -100,
+      currencyCode: "EUR",
+      period: "monthly",
+    }),
+    ).toThrow('Incorrect salary value');
+  });
+
+  it("should throw error for invalid salary value zero case", () => {
+    expect(() => calculateProposedSalary({
+          value: 0,
+          currencyCode: "EUR",
+          period: "monthly",
+        }),
+    ).toThrow('Incorrect salary value');
+  });
+
+  it("should throw error for invalid salary currency code", () => {
+    expect(() => calculateProposedSalary({
+          value: 50000000,
+          currencyCode: "INR",
+          period: "weekly",
+        }),
+    ).toThrow('Incorrect currency code');
+  });
+
+  it("should throw error for invalid salary invalid period", () => {
+    expect(() => calculateProposedSalary({
+          value: 500,
+          currencyCode: "EUR",
+          period: "invalid_period",
+        }),
+    ).toThrow('Incorrect period');
   });
 });
 
@@ -41,5 +77,69 @@ describe("calculateTotalSalary", () => {
       currencyId: "978",
       periodId: 4,
     });
+  });
+
+  it("should throw an error if bonus value is invalid", () => {
+    const baseSalary = {
+      value: 3000.5,
+      currencyId: "978",
+      periodId: 4,
+    };
+
+    const bonuses = [
+      { value: 500, currencyId: "978" },
+      { value: -200, currencyId: "978" },
+    ];
+    expect(() => calculateTotalSalary(baseSalary, bonuses)).toThrow(
+        "Incorrect bonus value",
+    )
+  });
+
+  it("should throw an error if bonus value is not a number", () => {
+    const baseSalary = {
+      value: 3000.5,
+      currencyId: "978",
+      periodId: 4,
+    };
+
+    const bonuses = [
+      { value: 500, currencyId: "978" },
+      { value: 'NaN', currencyId: "978" },
+    ];
+    expect(() => calculateTotalSalary(baseSalary, bonuses)).toThrow(
+        "Incorrect bonus value",
+    )
+  });
+
+  it("should throw an error if bonus value null", () => {
+    const baseSalary = {
+      value: 3000.5,
+      currencyId: "978",
+      periodId: 4,
+    };
+
+    const bonuses = [
+      { value: 500, currencyId: "978" },
+      { value: 0, currencyId: "978" },
+    ];
+    expect(() => calculateTotalSalary(baseSalary, bonuses)).toThrow(
+        "Incorrect bonus value",
+    );
+  });
+
+  it("should throw an error if currency does not match base salary currency", () => {
+    const baseSalary = {
+      value: 3000.5,
+      currencyId: "978",
+      periodId: 4,
+    };
+
+    const bonuses = [
+      { value: 500, currencyId: "978" },
+      { value: 200, currencyId: "826" },
+    ];
+    expect(() => calculateTotalSalary(baseSalary, bonuses)).toThrow(
+        "Bonus currency must match base salary currency",
+    );
   });
 });
